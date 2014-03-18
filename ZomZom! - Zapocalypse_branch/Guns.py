2bd7c0100641a07d8ddb6b1b1c86b26f8dee40ed
gillstudio <gillebunchstudio@gmail.com>
2014-03-18 14:58:30 -0700

    UPDATE

--- ZomZom! - Zapocalypse_branch/Guns.py
+++ ZomZom! - Zapocalypse_branch/Guns.py
@@ -9,5 +9,13 @@ class gun:
         
 def InitializeGuns():
     pistol = gun
+    mk47 = gun
     pistol.CreateGun(gun, "Pistol", 2, False, 0)
+    mk47.CreateGun(gun, "MK-47", 2, True, 15)
+    print (pistol.name)
+    
+    
+def PrintMessage():
+    print ("HEY, I'M WORKING!")    
+    
     
